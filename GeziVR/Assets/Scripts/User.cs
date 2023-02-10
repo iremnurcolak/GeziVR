2cc88754f0069accd5708e2c0933924a9a605999
İremnur Çolak <iremnurcolak34@gmail.com>
2023-02-10 18:47:03 +0300

    FIR-42 User class'ından bir değişken ismi değiştirildi.

--- GeziVR/Assets/Scripts/User.cs
+++ GeziVR/Assets/Scripts/User.cs
@@ -4,13 +4,13 @@ public class User
     public string name;
     public string email;
     public string profileImageUrl;
-    public string googleToken;
+    public string userId;
 
-    public User(string name, string email, string profileImageUrl, string googleToken)
+    public User(string name, string email, string profileImageUrl, string userId)
     {
         this.name = name;
         this.email = email;
         this.profileImageUrl = profileImageUrl;
-        this.googleToken = googleToken;
+        this.userId = userId;
     }
 }
