f4b8302c59453f9aebface219ee8430e010ef537
İremnur Çolak <iremnurcolak34@gmail.com>
2023-03-08 23:28:07 +0300

    Resources folder yaratildi, eser prefableri oraya kondu, galeriye 3 boyutlu objeler dondurme ozelligiyle eklendi.

--- GeziVR/Assets/Scripts/RotationController.cs
+++ GeziVR/Assets/Scripts/RotationController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class RotationController : MonoBehaviour
 {
-    float rotSpeed = 20f;
-    
+    float rotSpeed = 10f;
+    //for development in editor
+    //float rotSpeed = 20f;
     public void OnMouseDrag()
     {
-        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+        //for development in editor
+        //float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        //float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+        float rotX = Input.GetTouch(0).deltaPosition.x * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetTouch(0).deltaPosition.y * rotSpeed * Mathf.Deg2Rad;
 
         transform.RotateAround(Vector3.up, -rotX);
         transform.RotateAround(Vector3.right, rotY);
     }
+  
 }
