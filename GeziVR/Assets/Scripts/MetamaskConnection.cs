39f5f3e1e7623347d03e49787eda4d635ff7c226
Kemal Demirel <kmldemirel99@gmail.com>
2023-03-19 16:34:33 +0300

    ChainSafe Added and Rinkeby testnet is used

---
+++ GeziVR/Assets/Scripts/MetamaskConnection.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Numerics;
+
+
+public class MetamaskConnection : MonoBehaviour
+{
+    
+    private string accountAddress = "0x91e59FEfBa20B898Ec6bECaFdE7Eb5774356e84F"; // Replace with your actual account address
+
+
+     void Start()
+    {   
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
