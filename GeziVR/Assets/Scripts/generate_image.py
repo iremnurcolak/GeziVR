45e9e516448ecc66cdfc1efde012f501366630e3
Bayram Mert Öney <bayrammertoney@gmail.com>
2023-04-09 16:14:48 +0300

    Script düzenlendi, materyal oluşturuldu.

--- GeziVR/Assets/Scripts/generate_image.py
+++ GeziVR/Assets/Scripts/generate_image.py
@@ -2,14 +2,29 @@ import openai
 import requests
 
 def generate_images():
+
     openai.api_key = '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'
+    idea_for_image = generate_idea_for_image()
+
     images = openai.Image.create(
-        prompt="random images",
-        n=1,
+        prompt=idea_for_image,
+        n=8,
         size="1024x1024"
     )
+    k = 1
     for image in images['data']:
-        save_image("deneme.jpeg", image['url'])
+        save_image("../GeneratedImages/img{}.png".format(k), image['url'])
+        k += 1
+
+def generate_idea_for_image():
+    openai.api_key = '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'
+    resp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "Random idea for a picture, start the sentence with 'Create'"}
+        ]
+    )
+    return resp['choices'][0]['message']['content']
 
 def save_image(file_path, url):
     img_data = requests.get(url).content
